c14c726b05c89e51670cd6549d751439b49f23f5
Unknown <daniel.zimmerman@gmail.com>
2018-07-31 13:39:52 -0600

    Fixed deleted request

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { send, json } from 'micro'
 import { router, get, post, put, del } from 'microrouter'
-const cors = require('micro-cors')()
 
+const cors = require('micro-cors')()
 import Contacts from './db/contacts'
 
 export default cors(router(
@@ -10,10 +10,14 @@ export default cors(router(
         await send(res, 200, results)
     }),
     post('/', async ( req, res) => {
-        console.log(`I'm Called`)
-
         const person = await json(req)
         const result = await Contacts.insert(person)
         return send (res, 201, result)
+    }),
+    del('/:id', async (req, res) => {
+        const id = req.params.id
+        console.log(id)
+        const result = await Contacts.remove({_id: id});
+        return send(res, 200, {ok: result > 0})
     })
 ))
